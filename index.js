ee5a0b3a7833b3df1a419bb5fd4edf005dfd35af
parmarshashank <parmarshashank6@gmail.com>
2024-10-02 12:08:46 +0530

    Replaced custom token generation with jsonwebtoken

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const jwt = require("jsonwebtoken");
 const users = [];
-
-function generateToken() {
-    const opts = ['a', 'b', 'c', 'd', 'e', 's', 's', 'y', 'x', '7', '3', 'y', 'n', 'm', 'j', 'h', 'o', '2', '7'];
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        token += opts[Math.floor(Math.random() * opts.length)]; // Fixed Math.random call
-    }
-    return token;
-}
+const JWT_SECRET = "thisisarandomjwtsecretkey"
+// function generateToken() {
+//     const opts = ['a', 'b', 'c', 'd', 'e', 's', 's', 'y', 'x', '7', '3', 'y', 'n', 'm', 'j', 'h', 'o', '2', '7'];
+//     let token = "";
+//     for (let i = 0; i < 32; i++) {
+//         token += opts[Math.floor(Math.random() * opts.length)]; 
+//     }
+//     return token;
+// }
 
 app.post('/signup', function (req, res) {
     const username = req.body.username;
@@ -35,15 +36,37 @@ app.post('/signin', function (req, res) {
     const password = req.body.password;
     const user = users.find(u => u.username === username);
     if (user && user.password === password) {
-        const token = generateToken();
-        user.token = token;
+        const token = jwt.sign({
+            username: username
+        }, JWT_SECRET);
+        // user.token = token;
+        //No need to store the token in the db as it is itself stateless
         res.json({
-            message: token,
+            token: token,
         });
     } else {
         res.status(403).json({ message: "The user does not exist or password is incorrect" }); 
     }
 });
+
+app.get("/me", function(req, res){
+    const token= req.headers.token;
+    const decodedInformation= jwt.verify(token, JWT_SECRET); //{ username: "snfjksnfjk"} converting jwt to the username
+    const username= decodedInformation.username;
+    
+    const user= users.find((u)=>u.username===username);
+    if(user){
+        res.json({
+            username: user.username,
+            password: user.password
+        });
+    }
+    else{
+        res.json({
+            message:"token invalid",
+        })
+    }
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
